f646aadf0e6e9469482037ddec11a182118d2ab6
Josh Weinstock <jweinstk@umich.edu>
2022-03-01 12:14:29 -0800

    substantial simplifying and code re-factoring

--- src/graph.jl
+++ src/graph.jl
@@ -2,37 +2,52 @@ struct interventionGraph
     g::MetaDiGraph
     nv::Int64
     data::DataFrame
-    hasIntervention::Bool
-    interventions::Union{Missing, Vector{String}}
+    x::Array{Float64}
+    donor_map::Dict{String, Int8}
+    donor::Array{Int8}
+    interventions::Array{Int8}
 end
 
-function interventionGraph(data::DataFrame, interventions::Vector{String})
-    default_weight = 0.0
-    nv = ncol(data) - 1
-    @info "$(now()) intervention graph has $nv nodes"
-    graph = interventionGraph(
-        # MetaDiGraph(complete_graph(nv), default_weight),
-        MetaDiGraph(SimpleGraph(nv), default_weight),
-        nv,
-        data,
-        length(interventions) >= 1 ? true : false,
-        interventions
-    )
-    colnames = names(data)
+# interventionGraph = interventionGraph3
 
-    for i in 1:nv
-        # @info "$(now()) node is $(data.readout_gene[i])"
-        # @info "$(now()) node is $(colnames[i])"
-        #   
-        col_index = i + 1
-        set_prop!(graph.g, i, :name, colnames[col_index])
-        if issubset(colnames[col_index], interventions)
-            n = inneighbors(graph, i)
-            for v in 1:n
-               rem_edge!(graph.g, n, i) # remove all incomming edges to intervened nodes 
+function interventionGraph(data::DataFrame)
+    default_weight = 0.0
+
+    targets = setdiff(names(data), ["donor", "intervention"])
+    nv = length(targets)
+    @info "$(now()) intervention graph has $nv nodes"
+    nrows = nrow(data)
+
+    x = Matrix(@view data[:, targets])
+    interventions = Int8.(zeros(nrows, nv))
+
+    unique_donors = unique(data.donor)
+    donor_map = Dict{String, Int8}()
+    donor = Array{Int8}(undef, nrows)
+
+    for i in 1:length(unique_donors)
+        donor_map[unique_donors[i]] = Int8(i)
+    end
+
+    for i in 1:nrows
+        donor[i] = donor_map[data.donor[i]]
+        for j in 1:nv
+            if data[i, :intervention] == targets[j]
+                interventions[i, j] = 1
+                @assert x[i, j] == 0.0
             end
         end
     end
+
+    graph = interventionGraph(
+        MetaDiGraph(SimpleDiGraph(nv), default_weight),
+        nv,
+        data,
+        x,
+        donor_map,
+        donor,
+        interventions
+    )
 
     @info "$(now()) done constructing graph"
     set_indexing_prop!(graph.g, :name)
@@ -46,63 +61,54 @@ function subset_to_interventions(data::DataFrame, intervention::String)
     return data[:, vnames]
 end
 
-struct interventionGraphSet
-    n_graphs::Int64
-    graphs::Array{interventionGraph}
-    data::DataFrame
-end
+function posterior_graph(parsed_chain::DataFrame, beta_threshold::Float64 = 0.02)
 
-function interventionGraphSet(data_collection::Dict, interventions::Vector{String})
-    n_interventions = length(interventions)
-    graph_collection = interventionGraphSet(
-        n_interventions,
-        Array{interventionGraph}(undef, n_interventions),
-        dropmissing(reduce(vcat, values(data_collection)))
-    )
-
-    for i in range(1, stop = n_interventions)
-        graph_collection.graphs[i] = interventionGraph(
-            dropmissing(data_collection[interventions[i]]), 
-            [interventions[i]]
-        )
-    end
-
-    return graph_collection
-end
-    
-struct posteriorSkeleton
-    g::MetaGraph
-    nv::Int64
-    nodes::Array{String}
-    skeleton::DataFrame
-end
-
-function posteriorSkeleton(parsed_skel::DataFrame, pip_threshold::Float64 = 0.01)
-    # skel = @view parsed_skel[parsed_skel.beta_pip .>= pip_threshold, :]
-    skel = @view parsed_skel[parsed_skel.beta_pip_crude .>= pip_threshold, :]
-    n_rows = nrow(skel)
-    nodes = unique(vcat(skel.x, skel.y))
+    filtered_chain = @view parsed_chain[abs.(parsed_chain.estimate) .>= beta_threshold, :]
+    n_rows = nrow(filtered_chain)
+    nodes = unique(vcat(filtered_chain.row, filtered_chain.col))
     n_vertices = length(nodes)
     n_edges = n_rows # including for clarity..
     @info "$(now()) Now constructing graph from $n_vertices nodes and $n_edges edges"
 
-    g = MetaGraph(SimpleGraph(n_vertices))
+    default_weight = 0.0
+    g = MetaDiGraph(SimpleDiGraph(n_vertices), default_weight)
 
     for i in 1:n_rows
-        x_node_index = findall(x -> x == skel.x[i], nodes)[1]
-        y_node_index = findall(x -> x == skel.y[i], nodes)[1]
+        x_node_index = findall(x -> x == filtered_chain.row[i], nodes)[1]
+        y_node_index = findall(x -> x == filtered_chain.col[i], nodes)[1]
         add_edge!(g, x_node_index, y_node_index)
-        set_prop!(g, Edge(x_node_index, y_node_index), :weight, parsed_skel.beta_x[i])
+        set_prop!(g, Edge(x_node_index, y_node_index), :weight, filtered_chain.estimate[i])
     end
 
     for i in 1:n_vertices
         set_prop!(g, i, :name, nodes[i])
     end
 
-    return posteriorSkeleton(
-        g,
-        n_vertices,
-        nodes,
-        skel
-    )
+    return g
+end
+
+function posteriorGraph!(graph::interventionGraph, parsed_chain::DataFrame, beta_threshold::Float64 = 0.02)
+    filtered_chain = @view parsed_chain[abs.(parsed_chain.estimate) .>= beta_threshold, :]
+    n_rows = nrow(filtered_chain)
+    nodes = unique(vcat(filtered_chain.row, filtered_chain.col))
+    n_vertices = length(nodes)
+    n_edges = n_rows # including for clarity..
+    @info "$(now()) Now constructing graph from $n_vertices nodes and $n_edges edges"
+
+    for i in 1:n_rows
+        x_node_index = findall(x -> x == filtered_chain.row[i], nodes)[1]
+        y_node_index = findall(x -> x == filtered_chain.col[i], nodes)[1]
+        add_edge!(graph.g, x_node_index, y_node_index)
+        set_prop!(graph.g, Edge(x_node_index, y_node_index), :weight, filtered_chain.estimate[i])
+    end
+
+    for i in 1:n_vertices
+        set_prop!(graph.g, i, :name, nodes[i])
+    end
+
+    for i in (n_vertices + 1):nv(graph.g)
+        # TODO: need to run this multiple times??
+        rem_vertex!(graph.g, i) || error("failed to remove node $i") # remove superflous nodes
+    end
+
 end
