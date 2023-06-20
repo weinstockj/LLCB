97cca2581941030c2d83e216034b7cfeee5be0aa
Josh Weinstock <jweinstk@stanford.edu>
2023-06-20 11:45:34 -0700

    adding in LLC related data structures

--- src/graph.jl
+++ src/graph.jl
@@ -55,8 +55,9 @@ function interventionGraph(data::DataFrame)
     return graph
 end
 
-function subset_to_interventions(data::DataFrame, intervention::String)
-    vnames = names(data)[contains.(names(data), intervention)]
+function subset_to_interventions(data::DataFrame, metadata::DataFrame, intervention::String)
+    # vnames = names(data)[contains.(names(data), intervention)]
+    vnames = metadata.Sample[metadata.KO .== intervention]
     push!(vnames, "readout_gene")
     return data[:, vnames]
 end
@@ -259,13 +260,62 @@ function scale_control_sds(g::interventionGraph, x::Matrix{Float64})
     return x ./ sds
 end
 
-function estimate_total_effects(g::interventionGraph, log_normalize, center=true, scale=true)
+struct TotalEffects
+    readout_gene::Array{String}
+    ko_gene::Array{String}
+    effect::Array{Float64}
+end
+
+function TotalEffects(summary_stats::DataFrame)
+    TotalEffects(
+        summary_stats.readout_gene,
+        summary_stats.ko_gene,
+        summary_stats.log2FoldChange
+    )
+end
+
+function get_effect(e::TotalEffects, readout_gene::String, ko_gene::String)
+    indx::BitVector = (e.readout_gene .== readout_gene) .& (e.ko_gene .== ko_gene)
+    effect::Float64 = only(view(e.effect, indx)) # pointer to same value
+    return effect
+end
+
+function estimate_total_effects(g::interventionGraph, summary_stats::TotalEffects)
+    targets = setdiff(names(g.data), ["donor", "intervention"])
+
+    nv = length(targets)
+    total_effects = zeros(nv, nv)
+
+    @info "$(now()) now processing total effect estimates"
+
+    @simd for i in 1:nv
+        @inbounds for j in 1:nv
+            # if i == j
+            #     continue
+            # else
+                # row = (summary_stats.readout_gene .== targets[j]) .& (summary_stats.ko_gene .== targets[i])
+                # total_effects[i, j] = only(summary_stats[row, "log2FoldChange"])
+            total_effects[i, j] = get_effect(summary_stats, targets[j], targets[i])
+            # end
+            
+        end
+    end
+
+    total_effects[diagind(total_effects)] .= 0.0
+
+    @info "$(now()) done processing total effect estimates"
+
+    return total_effects
+end
+
+function estimate_total_effects(g::interventionGraph, log_normalize::Bool, center=true, scale=true)
     targets = setdiff(names(g.data), ["donor", "intervention"])
     experiment_observations = get_experiment_observations(g)
     
     nv = length(targets)
     ndonors = length(unique(g.donor))
 
+    # rows are parents, columns readouts
     total_effects = zeros(nv, nv)
 
     x = deepcopy(g.x)
@@ -286,11 +336,13 @@ function estimate_total_effects(g::interventionGraph, log_normalize, center=true
             if i == j
                 continue
             else
+                println("parent = $(targets[i]), child = $(targets[j])") 
                 total_effects[i, j] = linear_regress(
-                    hcat(
-                        x[experiment_observations[i], i], 
-                        Float64.(one_hot(g.donor)[experiment_observations[i], 1:(ndonors - 1)]) # remove last column for reference coding
-                    ), 
+                    # hcat(
+                    #     x[experiment_observations[i], i], 
+                    #     Float64.(one_hot(g.donor)[experiment_observations[i], 1:(ndonors - 1)]) # remove last column for reference coding
+                    # ), 
+                    x[experiment_observations[i], i], 
                     x[experiment_observations[i], j]
                 )
             end
@@ -300,9 +352,9 @@ function estimate_total_effects(g::interventionGraph, log_normalize, center=true
     return total_effects
 end
 
-function get_cyclic_matrices(g::interventionGraph, log_normalize = true, center = true, scale = true)
+function get_cyclic_matrices(g::interventionGraph, summary_stats::TotalEffects)
 
-    total_effects = estimate_total_effects(g, log_normalize, center, scale)
+    total_effects = estimate_total_effects(g, summary_stats)
 
     T = zeros(g.nv * (g.nv - 1), g.nv * (g.nv - 1))
     t = zeros(g.nv * (g.nv - 1))
@@ -312,7 +364,7 @@ function get_cyclic_matrices(g::interventionGraph, log_normalize = true, center 
     edges = Vector{Pair}()
 
     # for perturbed in 1:g.nv
-        for observed in 1:g.nv
+        @inbounds for observed in 1:g.nv
             # if observed == perturbed
             #     continue
             # else
@@ -333,7 +385,69 @@ function get_cyclic_matrices(g::interventionGraph, log_normalize = true, center 
                 T[experiment_idx, experiment_idx] = Tᵥ
                 t[experiment_idx] = total_effects[1:g.nv .!= observed, observed]
 
-                for e in 1:g.nv
+                @inbounds for e in 1:g.nv
+                    if e != observed
+                        push!(edges, Pair(e, observed))
+                    end
+                end
+                
+
+                # row += 1 # this row includes the various experimental effects of a single gene
+                # col = 0
+                # for l in 1:g.nv
+                #     for m in 1:g.nv
+                #         if l == m 
+                #             continue
+                #         else
+                #             col += 1
+                #             if perturbed == l 
+                #                 T[row, col] = total_effects[l, m]
+                #             end
+                #         end
+                #     end
+                # end
+                # t[row] = total_effects[perturbed, observed]
+            # end
+        # end
+    end
+
+    return T, t, edges
+end
+
+function get_cyclic_matrices(g::interventionGraph, log_normalize::Bool, center=true, scale=true)
+
+    total_effects = estimate_total_effects(g, log_normalize, center, scale)
+
+    T = zeros(g.nv * (g.nv - 1), g.nv * (g.nv - 1))
+    t = zeros(g.nv * (g.nv - 1))
+    row = 0
+    col = 0
+
+    edges = Vector{Pair}()
+
+    # for perturbed in 1:g.nv
+        @inbounds for observed in 1:g.nv
+            # if observed == perturbed
+            #     continue
+            # else
+                # experiment_start = (observed - 1 - Int(observed > perturbed)) * g.nv + 1
+                experiment_start = (observed - 1) * (g.nv - 1) + 1
+                experiment_end = experiment_start + (g.nv - 1) - 1
+                experiment_idx = experiment_start:experiment_end
+                @assert length(experiment_idx) == g.nv - 1
+                # println("observed = $observed, experiment idx = $experiment_idx")
+                # Tᵥ = zeros(g.nv, g.nv)
+                # for i in 1:g.nv
+                #     for j in 1:g.nv
+                #         Tᵥ[i, j] = total_effects[i, j]
+                #     end
+                # end
+                Tᵥ = total_effects[1:g.nv .!= observed, 1:g.nv .!= observed]
+                Tᵥ[diagind(Tᵥ)] .= 1.0
+                T[experiment_idx, experiment_idx] = Tᵥ
+                t[experiment_idx] = total_effects[1:g.nv .!= observed, observed]
+
+                @inbounds for e in 1:g.nv
                     if e != observed
                         push!(edges, Pair(e, observed))
                     end
