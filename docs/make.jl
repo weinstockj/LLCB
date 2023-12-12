7c16517a865bb30f7e6b9b81d0875f73468ff254
Josh Weinstock <jweins17@jhu.edu>
2023-12-12 17:17:53 -0500

    adding documentation folder

---
+++ docs/make.jl
@@ -0,0 +1,13 @@
+# Inside make.jl
+push!(LOAD_PATH,"../src/")
+using InferCausalGraph
+using Documenter
+makedocs(
+         sitename = "InferCausalGraph.jl",
+         modules  = [InferCausalGraph],
+         pages=[
+                "Home" => "index.md"
+               ])
+deploydocs(;
+    repo="github.com/weinstockj/LLCB.jl",
+)
